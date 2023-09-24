cde3ec320191eebe6d88707281e2ec51df5ee41a
Adam McCall <adam.max.mccall@gmail.com>
2023-09-23 19:26:32 -0600

    Basic menu functionality

---
+++ Assets/Scripts/SceneButtons.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneButtons : MonoBehaviour
+{
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
